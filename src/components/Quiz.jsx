2b5f0ebabd1496d4c989b31f432579d4ae39629d
davutbilgicnl <davutbilgic.nl@gmail.com>
2023-12-29 11:40:51 +0100

    Outputting Quiz Result

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useCallback, useRef, useState } from "react";
 
 import QUESTIONS from "../questions";
-import quizComleteImage from "../assets/quiz-complete.png";
 import Question from "./Question";
+import Summary from "./Summary";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -20,12 +20,7 @@ export default function Quiz() {
   const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizComleteImage} alt="Tropy icon" />
-        <h2></h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
