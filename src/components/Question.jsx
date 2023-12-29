8b758764ab26f0186274257d7478efbe14354f61
davutbilgicnl <davutbilgic.nl@gmail.com>
2023-12-29 11:03:20 +0100

    Setting Different Timers Based On The Selected Answer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -6,6 +6,16 @@ import QUESTIONS from "../questions";
 export default function Question({ questionIndex, onSelectAnswer, onSkipAnswer }) {
   const [answer, setAnswer] = useState({ selectedAnswer: "", isCorrect: null });
 
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
   function handleSelectAnswer(answer) {
     setAnswer({
       selectedAnswer: answer,
@@ -34,7 +44,12 @@ export default function Question({ questionIndex, onSelectAnswer, onSkipAnswer }
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[questionIndex].text}</h2>
       <Answers
         answers={QUESTIONS[questionIndex].answers}
